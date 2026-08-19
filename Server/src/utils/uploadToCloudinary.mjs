b8c63a55ee4315eae18ab003428019dfb6fa9c26
Uzair Ahmed <uzairahmedone@gmail.com>
2026-08-19 22:49:41 +0500

    added utily function for cloudinary upload

---
+++ Server/src/utils/uploadToCloudinary.mjs
@@ -0,0 +1,25 @@
+import cloudinary from "../config/cloudinary.mjs";
+import streamifier from "streamifier";
+
+const uploadToCloudinary = (fileBuffer) => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                folder: "eventsphere/events"
+            },
+            (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+
+        streamifier
+            .createReadStream(fileBuffer)
+            .pipe(stream);
+    });
+};
+
+export default uploadToCloudinary;
